e2e1ff6efaf95e793997ad7025557d9c60202200
RicardoRam781 <alexrp782@gmail.com>
2023-04-12 22:28:30 -0600

    add corrections to deploy

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -6,12 +6,8 @@ import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Link , useNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 export default function TemporaryDrawer() {
   const [state, setState] = React.useState({
     top: false,
