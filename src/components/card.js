83704937abd6337f25f206b2ed23f7fd7bcadfa5
Alejandro Pérez <alexrp782@gmail.com>
2023-04-14 20:06:15 -0600

    add routes corrections

--- src/components/card.js
+++ src/components/card.js
@@ -14,7 +14,7 @@ export default function RenderProduct() {
     }, [])
 
     const getData = async () => {
-        const res = await fetch('http://localhost:4000/inicio') // http://localhost:4000
+        const res = await fetch('https://novedades-rosy-api-production.up.railway.app/inicio') // http://localhost:4000
         const data = await res.json()
         console.log(data);
         setProduct(data)
